c8c8b733df1b6729c3d52fd56e9892a3b5cfae9e
MohanBabu2003 <141659590+MohanBabu2003@users.noreply.github.com>
2024-09-04 14:14:26 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+pi = 22 / 7
+print("Data type of pi:", type(pi))
+for_value = 4
+print("Value of for_value:", for_value)
+P = 1000  
+R = 5     
+T = 3     
+simple_interest = P * R * T / 100
+print("Simple Interest for 3 years:", simple_interest)
